c89e94fecbb027d30939a47a0a76af41c03b7c88
경호 <moltak@moltak-iMac.local>
2015-01-06 00:39:58 +0900

    일단 빌드 되게는 했당...

---
+++ app/src/main/java/com/limotaxi/androidtestintegration/model/User.java
@@ -0,0 +1,10 @@
+package com.limotaxi.androidtestintegration.model;
+
+/**
+ * Created by moltak on 15. 1. 6..
+ */
+public class User {
+    public int getLength() {
+        return 10;
+    }
+}
---
+++ app/src/mainTest/java/com/limotaxi/androidtestintegration/UserTest.java
@@ -0,0 +1,16 @@
+package com.limotaxi.androidtestintegration.model;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.Before;
+/**
+ * Created by moltak on 15. 1. 6..
+ */
+public class UserTest {
+    @Test
+    public void testShouldHave10Length() {
+        User user = new User();
+        Assert.assertEquals(10, user.getLength());
+    }
+}
